9cdf43cb361b4d4c68083611f31cb8f70f2ed254
Artem <artichek123@gmail.com>
2024-10-06 20:09:47 +0300

    Refactor code for tg and coin prices

---
+++ client/service/coin_price_service.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import requests
+
+
+class CoinPriceService:
+    def __init__(self, base_url='https://api.coingecko.com/api/v3'):
+        self.base_url = base_url
+
+    def get_coin_price_history(self, coin_id: str, vs_currency: str = 'usd', days: int = 30) -> pd.DataFrame:
+        """
+        Fetch historical price data for a given id from CoinGecko.
+
+        Args:
+            coin_id (str): Cryptocurrency id (e.g., 'bitcoin').
+            vs_currency (str): Currency to compare against (e.g., 'usd').
+            days (int): Number of days for historical data.
+
+        Returns:
+            pd.DataFrame: DataFrame containing price data.
+        """
+        url = f'https://api.coingecko.com/api/v3/coins/{coin_id}/market_chart'
+        params = {'vs_currency': vs_currency, 'days': days, 'interval': 'daily'}
+        response = requests.get(url, params=params)
+        if response.status_code != 200:
+            raise Exception(f"Error fetching data: {response.status_code}")
+
+        data = response.json()
+
+        if 'prices' in data:
+            prices = data['prices']
+            df = pd.DataFrame(prices, columns=['timestamp', 'price'])
+            df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+            df.set_index('timestamp', inplace=True)
+            return df
+        else:
+            print(f"No price data available for {coin_id}.")
+            return pd.DataFrame(columns=['timestamp', 'price'])
+
+
+if __name__ == '__main__':
+    coin_service = CoinPriceService()
+
+    df = coin_service.get_coin_price_history('hamster', days=90)
+    print(df)
--- telegram_server/telegram_bot.py
+++ telegram_server/telegram_bot.py
@@ -3,36 +3,36 @@ import os
 import telebot
 from dotenv import load_dotenv
 
-from client.coin_client import get_coin_price_history
+from client.service.coin_price_service import CoinPriceService
 
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-print(TELEGRAM_TOKEN)
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, 'Hello! Use /price <symbol> to get daily price data, e.g., /price hamster')
+    bot.reply_to(message, 'Hello! Use /price <id> to get daily price data, e.g., /price hamster')
 
 
 @bot.message_handler(commands=['price'])
 def send_price(message):
     try:
-        symbol = message.text.split()[1].lower()
+        id = message.text.split()[1].lower()
     except IndexError:
-        bot.reply_to(message, "Please specify a cryptocurrency symbol, e.g., /price hamster.")
+        bot.reply_to(message, "Please specify a cryptocurrency id, e.g., /price hamster.")
         return
 
     days = 7  # Fetch data for the past 7 days
-    df = get_coin_price_history(symbol, days=days)
+    coin_service = CoinPriceService()
+    df = coin_service.get_coin_price_history(id, days=days)
 
     if df is not None:
         prices_text = df['price'].tail(days).to_string()
-        bot.reply_to(message, f"Daily prices for {symbol}:\n{prices_text}")
+        bot.reply_to(message, f"Daily prices for {id}:\n{prices_text}")
     else:
-        bot.reply_to(message, f"Could not retrieve data for {symbol}. Please try again.")
+        bot.reply_to(message, f"Could not retrieve data for {id}. Please try again.")
 
 
 if __name__ == '__main__':
