258a25c140f29cff5d6809d1c3f4e7ef35e73628
Konstantin.Grotov <konstantin.grotov@gmail.com>
2024-11-06 20:39:13 +0100

    add linter and fix the code

--- src/client/app.py
+++ src/client/app.py
@@ -3,14 +3,18 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class UserInput(BaseModel):
     user_input: str
+
 
 @app.get("/get-input")
 def get_input():
     user_input = input("Please enter something: ")
     return {"user_input": user_input}
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
